45bd3adf20365bdbee48345be75b075059b28852
nathalia <nathaliatroina@gmail.com>
2022-08-11 11:46:57 -0400

    routes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,27 @@
+import React, {useState, useEffect} from 'react'
 import key from '../../key'
+import axios from 'axios'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
+
+  let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q=thesimplewild&printType=books&key=' + key
+  
+  let [testData, setTestData] = useState([])
+  useEffect(() => {
+    axios.get(booksUrl)
+    .then(r => setTestData(r.data))
+  },[])
+
   return (
     <div>
-      
+    <Routes>
+    <Route path= '/*' element={
+      <div> ola</div>
+    }
+
+    />
+    </Routes>
     </div>
   );
 }
