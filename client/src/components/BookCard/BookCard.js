ce9b497d7b843d02c4445cbd22750ad6e20f9ce0
nathalia <nathaliatroina@gmail.com>
2022-08-12 11:58:27 -0400

    display all the books

--- client/src/components/BookCard/BookCard.js
+++ client/src/components/BookCard/BookCard.js
@@ -1,8 +1,32 @@
 
 
 
-export default function BookCard(){
+export default function BookCard( {book} ){
+
+    //I need book cover, book title and author to display on the browser
+    let bookCover = book.volumeInfo.imageLinks.thumbnail
+    //console.log(bookCover)
+
+    //Toda vez que o tittulo e muito grande ele empurra o author pra baixo tirando do card. Para evitar isso, vou fazer a ternary
+    let bookTitle = book.volumeInfo.title.length > 35 ? book.volumeInfo.title.slice(0,20)+ '...' : book.volumeInfo.title
+
+    //console.log(bookTitle)
+    let bookAuthor = book.volumeInfo.authors[0]
+    //console.log(bookAuthor)
+
+
+    
+
+
     return(
-        <div></div>
+            <div className='bookCard' >
+                <div className='bookCardHalf top'>
+                <img className='bookCover' src={bookCover} alt='' />
+                    </div>
+                <div className='bookCardHalf bottom' >
+                <div className='bookTitle'>  {bookTitle} </div>
+                <div className='bookAuthor'> by: {bookAuthor} </div>
+                </div>
+            </div>
     )
 }
--- client/src/components/BookContainer/BookContainer.js
+++ client/src/components/BookContainer/BookContainer.js
@@ -1,3 +1,4 @@
+import BookCard from "../BookCard/BookCard"
 
 
 
@@ -5,32 +6,20 @@ export default function BookContainer({ testData }) {
     if (!testData.items) {
         return null
     }
-    let firstBook = testData.items[0]
-    //console.log(firstBook)
-    //I need book cover, book title and author to display on the browser
-    let bookCover = firstBook.volumeInfo.imageLinks.thumbnail
-    //console.log(bookCover)
-
-    let bookTitle = firstBook.volumeInfo.title
-    //console.log(bookTitle)
-
-    let bookAuthor = firstBook.volumeInfo.authors
-    //console.log(bookAuthor)
-
-
+     let booksToDisplay = testData.items.map(book => {
+        return (
+            <BookCard
+            key= {book.id}
+            book={book}
+            />
+        )
+     })
+   
 
     return (
 
         <div className='bookContainer' > 
-            <div className='bookCard' >
-                <div className='bookCardHalf top'>
-                <img className='bookCover' src={bookCover} alt='' />
-                    </div>
-                <div className='bookCardHalf bottom' >
-                <div className='bookTitle'>  {bookTitle} </div>
-                <div className='bookAuthor'> by: {bookAuthor} </div>
-                </div>
-            </div>
+        {booksToDisplay}
         </div>
     )
 }
