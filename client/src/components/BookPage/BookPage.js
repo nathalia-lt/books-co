21503e39b904178b80c53cc674d00b9426020529
nathalia <nathaliatroina@gmail.com>
2022-08-15 11:58:57 -0400

    bookpage style

--- client/src/components/BookPage/BookPage.js
+++ client/src/components/BookPage/BookPage.js
@@ -1,7 +1,36 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import key from "../../key"
+import axios from "axios"
 
 
-export default function BookPage(  ){
-    return(
-        <div></div>
+export default function BookPage() {
+
+//Fetching the specific book for the bookPage
+
+    let params = useParams() //take any think that is after the colon ('/book/:id'). after :
+    //we are going to use this ID to performance another fetch to give us the information about the book we selected
+    console.log(params)
+    let id = params.id
+
+    let [pageData, setPageData] = useState({})
+
+    let url = "https://www.googleapis.com/books/v1/volumes/" + id + '?&key=' + key //I took this link API
+
+    useEffect(() => {
+        axios.get(url)
+            .then(r => setPageData(r.data))
+    }, [])
+
+
+
+
+
+
+    return (
+        <div className='mainContainer' >
+            <div className='sideBar'  > sidebar</div>
+            <div className='display' > display</div>
+        </div>
     )
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,10 +2,13 @@ import { Axios } from "axios"
 import React, { useState } from "react"
 import key from "../../key"
 import axios from "axios"
+import { useNavigate } from "react-router-dom";
+
 
 
 
 export default function Header( {setTestData} ) {
+    let navigate = useNavigate() //I use useNavigate to send me to a new page
 
     let [searchTerm, setSearchTerm] = useState('')
     
@@ -21,11 +24,17 @@ export default function Header( {setTestData} ) {
         .then(r => setTestData(r.data))
     }
 
+    //----------------------------------------
+    //I will make a function for when I click on the title I am sent back to the home page
+
+    function handleHomeClick(){
+        navigate('/')
+    }
 
     return (
         <div className='header'>
             <div> Clubs </div>
-            <div> Header </div>
+            <div onClick={handleHomeClick} className= 'title'> Header </div>
             <form onSubmit={handleSearchSubmit} >
                 <input className='searchBar'
                 type= 'text'
