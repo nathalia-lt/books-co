04b6fbab2d8d6af1bc19a1c2e10023110ee5c0a7
nathalia <nathaliatroina@gmail.com>
2022-08-16 13:29:31 -0400

    function search page component

---
+++ client/src/components/SearchPage/SearchPage.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import BookContainer from "../BookContainer/BookContainer";
+import key from "../../key"
+import axios from "axios"
+
+
+
+export default function SearchPage( {searchResults, setSearchResults} ){
+    let params = useParams()
+    let searchTerm = params.searchTerm
+
+    let booksUrl = 'https://www.googleapis.com/books/v1/volumes?q='+searchTerm+'&maxResults=30&printType=books&key=' + key
+
+
+    useEffect(() => {
+        axios.get(booksUrl)
+        .then(r => {
+                setSearchResults(r.data)
+            })
+    },[searchTerm]) //wherever we put in the squares brackets, useEffect runs again
+
+
+    if (!searchResults.items) return null
+
+
+
+    return(
+        <div>
+            <BookContainer
+            testData={searchResults}
+            />
+        </div>
+    ) 
+}
