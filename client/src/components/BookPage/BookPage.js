0d6ae36e1b69a1a8a6895b2d9d2b45e448f19eee
nathalia <nathaliatroina@gmail.com>
2022-08-17 12:54:12 -0400

    stars rating

--- client/src/components/BookPage/BookPage.js
+++ client/src/components/BookPage/BookPage.js
@@ -6,12 +6,13 @@ import FeaturedBook from "../FeaturedBook/FeaturedBook"
 
 
 export default function BookPage() {
+    let [hoverStars, setHoverStars] = useState(0)
+    let [clickedStars, setClickedStars] = useState(0)
 
     //Fetching the specific book for the bookPage
 
     let params = useParams() //take any think that is after the colon ('/book/:id'). after :
     //we are going to use this ID to performance another fetch to give us the information about the book we selected
-    console.log(params)
     let id = params.id
 
     let [pageData, setPageData] = useState({})
@@ -29,16 +30,77 @@ export default function BookPage() {
     }
 
 
+    //---------------------------------------------------------------
+
+    //rating star code:
+
+    //☆★
+
+    function handleClickStar(num) {
+        if (hoverStars === num && clickedStars) {
+            setClickedStars(0)
+            setHoverStars(num)
+        } else {
+            setClickedStars(num)
+            setHoverStars(num)
+        }
+    }
+
+    function handleHoverStar(num) {
+        setHoverStars(num)
+    }
+
+    function handleHoverStarOff() {
+        setHoverStars(clickedStars)
+    }
+
+    function displayStars(num) {
+        return clickedStars >= num || hoverStars >= num ? '★' : '☆'
+    }
+
+
+
+
+
 
 
     return (
         <div className='mainContainer' >
             <div className='sideBar'  > sidebar</div>
             <div className='display' >
-            <FeaturedBook
-            book={pageData}
-            />
+                <FeaturedBook
+                    book={pageData}
+                />
+                <div className='reviewContainer' >
+                    <div className='reviewInfo'>
+                        <h2>Average Rating: 4.5 ⭐️</h2>
+                        <h4>Number of ratings: 10</h4>
+                        <hr></hr>
+                        <div className="newReviewTitle">
+                            <label>Write a Review: </label>
+                            <div className="stars" onMouseOut={handleHoverStarOff}>
+                                {/* I make an anonymous function because all functions that are placed in an element's attribute must not have inputs */}
+                                {/* if you place a function in an element's attribute (onClick,onMouseOver,etc) */}
+                                {/* and it requires an input (like in the function handleClickStar) */}
+                                {/* that function will automatically be invoked */}
+                                {/* the solution is to make another function and place the desired function in it  */}
+                                <div onClick={() => { handleClickStar(1) }} onMouseOver={() => handleHoverStar(1)}>{displayStars(1)}</div>
+                                <div onClick={() => { handleClickStar(2) }} onMouseOver={() => handleHoverStar(2)}>{displayStars(2)}</div>
+                                <div onClick={() => { handleClickStar(3) }} onMouseOver={() => handleHoverStar(3)}>{displayStars(3)}</div>
+                                <div onClick={() => { handleClickStar(4) }} onMouseOver={() => handleHoverStar(4)}>{displayStars(4)}</div>
+                                <div onClick={() => { handleClickStar(5) }} onMouseOver={() => handleHoverStar(5)}>{displayStars(5)}</div>
+                            </div>
+                        </div>
+                        <form>
+                            <input
+                                type='text'
+                            />
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
