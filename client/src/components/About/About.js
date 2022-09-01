2a2f02858d3c3435c175bc81b864e8606aec9c56
nathalia <nathaliatroina@gmail.com>
2022-09-01 09:33:32 -0400

    fixing about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -11,7 +11,7 @@ import postresql from './postresql.png'
 export default function About() {
     document.title = 'About'
 
-    
+
 
     function handleGitHubClick() {
         window.open('https://github.com/nathalia-lt/books-co')
@@ -26,26 +26,31 @@ export default function About() {
             <div className='aboutPage' >
                 <div className='aboutInformation'>
 
-                <h1 className= 'aboutTitle'>  About </h1>
-                <p className= 'paragraph'> Books & Co. is a website where the users can find their favorites books.
-                    You can search by book, author or genre, save favorites, and join a book club.
+                    <h1 className='aboutTitle'>  About </h1>
+                    <p className='paragraph'> Books & Co. is a website where the users can find their favorites books.
+                        You can search by book, author or genre, save favorites, and join a book club.
                     </p>
-                <h4> Created by Nathalia Troina</h4>
-                <h4> Made With: </h4>
-                <div className= 'icons js'>
-                    <img src={javascript} alt='' />
-                    <img src={react} alt='' />
-                    <img src={sass} alt='' />
-                    <img src={ruby} alt='' />
-                    <img src={postresql} alt='' />
-                </div>
-                <h4> For More Information: </h4>
-                <div className= 'icons' >
-                    <img  onClick={handleGitHubClick} src={github} alt='' />
-                    <img onClick={handleLinkedinClick} src={linkedin} alt='' />
+                    <h4> Created by:</h4>
+                    <div className='createdby'>
+                        <li>Nathalia Troina</li>
+                        <li>Henry Miller</li>
+                    </div>
+
+                    <h4> Made With: </h4>
+                    <div className='icons js'>
+                        <img src={javascript} alt='' />
+                        <img src={react} alt='' />
+                        <img src={sass} alt='' />
+                        <img src={ruby} alt='' />
+                        <img src={postresql} alt='' />
+                    </div>
+                    <h4> For More Information: </h4>
+                    <div className='icons' >
+                        <img onClick={handleGitHubClick} src={github} alt='' />
+                        <img onClick={handleLinkedinClick} src={linkedin} alt='' />
+                    </div>
                 </div>
             </div>
-                </div>
         </div>
     )
 }
